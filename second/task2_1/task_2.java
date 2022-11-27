c342cf2d80aa373b9a4a2ad816fa326a1791ac87
vit21513 <vit21513@yandex.ru>
2022-11-27 10:13:04 +0300

    add_remark

--- second/task2_1/task_2.java
+++ second/task2_1/task_2.java
@@ -7,69 +7,43 @@ import java.io.*;
 public class task_2 {
     public static void main(String[] args) {
 
-        
-        
-        int [] arr = new int[8];                                 //обьявляем массив размером 
-        for (int index = 0;  index <arr.length ; index++) {
-             arr[index] = getRandomNumber();  }
-
-            
+        int[] arr = new int[8]; // обьявляем массив размером
+        for (int index = 0; index < arr.length; index++) {
+            arr[index] = getRandomNumber();
+        }
 
         for (int i = 0; i < arr.length; i++) {
             logArray(arr);
-            System.out.print("\n");
-        for (int j = 0; j < arr.length-1; j++) {
-                if(arr[j + 1] < arr[j]) {
+            
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j + 1] < arr[j]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;               
-                
+                    arr[j + 1] = temp;
+
+                }
             }
         }
-    
-        }}
-        
-    
+    }
 
-
-    public static int getRandomNumber() {                     //генерирует случайное число
+    public static int getRandomNumber() { // генерирует случайное число
         double x = (Math.random() * 99);
         int num = (int) x;
-        return num;}
+        return num;
+    }
 
+    public static void logArray(int[] array) {
 
+        String arrayString = Arrays.toString(array);
+        System.out.print(arrayString); // выводит в виде строки массив
+        System.out.print("\n");
 
-    public static void logArray(int []array) {
-
-        String arrayString = Arrays.toString(array);                   
-        System.out.print(arrayString);                     // выводит в виде строки массив
-
-        try(FileWriter writer = new FileWriter("log.txt", true))   
-        {       
-            writer.append(arrayString); //дописывает в файл итеррации сортировки
+        try (FileWriter writer = new FileWriter("log.txt", true)) {
+            writer.append(arrayString); // дописывает в файл итеррации сортировки
             writer.append("\n");
             writer.flush();
+        } catch (IOException ex) {
+
         }
-        catch(IOException ex){
-
-
-
-
-
-        
-    } 
-        }}
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
+    }
+}
